88dcd7259690433e1e7b134a617871beaa899a97
Mahesh Ummidi <125633083+ummidimahesh012@users.noreply.github.com>
2025-07-21 06:13:18 +0000

    inputProcessor class added

---
+++ inputProcessor.cs
@@ -0,0 +1,11 @@
+using System;
+public class InputProcessor{
+public void HandleInput(string input){
+if(long.TryParse(input,out long number)){
+Console.WriteLine($"Number: {number}");
+}
+else{
+  Console.WriteLine("Invalid number or number too large.");  
+}
+}
+}
